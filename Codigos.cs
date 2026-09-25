c76646ba2ec5ead951e8a23c470468f4282a59bb
andersonlaguna07-pixel <andersonlaguna07@gmail.com>
2026-09-25 14:05:18 -0500

    Update Codigos.cs

--- Codigos.cs
+++ Codigos.cs
@@ -10,15 +10,19 @@ namespace A
     {
         static void Main(string[] args)
         {
-            int[] codigos = { 121, 263, 465, 218, 101, 589, 643, 888 };
+            int[] codigos = { 135, 247, 389, 412, 526, 638, 741, 853, 964, 176,
+                              285, 394, 507, 619, 728, 836, 945, 153, 267, 478 };
+
             Console.WriteLine($"codigos actuales: {string.Join(",", codigos)}");
-            Console.WriteLine($"Ingrese el codigo que desea actualizar: ");
-            int busqueda = Convert.ToInt32( Console.ReadLine() ); 
-            
+
+            Console.WriteLine("Ingrese el codigo que desea actualizar: ");
+            int busqueda = Convert.ToInt32(Console.ReadLine());
+
             int indiceEncontrado = -1;
+
             for (int i = 0; i < codigos.Length; i++)
             {
-                if (codigos[i] == busqueda) ;
+                if (codigos[i] == busqueda)
                 {
                     indiceEncontrado = i;
                     break;
@@ -28,8 +32,9 @@ namespace A
             if (indiceEncontrado != -1)
             {
                 Console.WriteLine("Ingrese el nuevo codigo: ");
-                codigos[indiceEncontrado] = Convert.ToInt32( Console.ReadLine() );
-                Console.WriteLine($"\nVector actualizado {string.Join(",", codigos)}");
+                codigos[indiceEncontrado] = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"\nVector actualizado: {string.Join(",", codigos)}");
             }
             else
             {
